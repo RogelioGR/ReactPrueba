b7e6067748a1dc811d7504f54c6a0919af1854fb
Rogelio García Ramos <124382204+RogelioGR@users.noreply.github.com>
2024-05-18 17:42:25 -0500

    Header y footer

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import ResponsiveAppBar from "./Componente/Header";
+import ColorInversionFooter from "./Componente/Footer";
+
+
+const App:any = ()=> {
+  return(
+    
+  <div className="App">
+    <ResponsiveAppBar/>
+    
+    <ColorInversionFooter/>
+
+  </div>
+  );
+  }
+  
+  
+  export default App;
